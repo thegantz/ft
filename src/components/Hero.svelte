70929e5533756f82bd8f2bb829a48f1c803b8322
gantz <jackiesc0tt@proton.me>
2024-03-21 14:11:40 +0100

    compressed img

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,7 +5,7 @@
     import Banner from "./Banner.svelte";
 </script>
 
-<div class="absolute sm:w-full min-h-screen md:bg-fixed bg-cover bg-center flex flex-col items-center py-10 md:bg-[url('/images/bg.png')]">
+<div class="absolute sm:w-full min-h-screen md:bg-fixed bg-cover bg-center flex flex-col items-center py-10 md:bg-[url('/images/bg2.png')]">
     <Logo />
     <Banner />
     <Teamcard />
